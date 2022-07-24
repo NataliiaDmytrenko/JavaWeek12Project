cb50cf09313c5b0e40d27de348b277c4daab4584
Nataliia Dmytrenko <ednata82@gmail.com>
2022-07-24 19:58:47 +0300

    Employee custom class example finished

--- src/constructor/Employee.java
+++ src/constructor/Employee.java
@@ -21,4 +21,14 @@ public class Employee {
             this.ID = ID;
             this.salary = salary;
     }
+
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", ID=" + ID +
+                ", salary=" + salary +
+                ", company Name=" + companyName+
+                '}';
+    }
 }
